9e6ba59461c24e32724a1f04fe82c0ddcd8c642d
zhangcm <justif_z@163.com>
2021-04-15 10:30:06 +0800

    字母异位词分组

---
+++ src/main/java/arithmetic/exercise/medium/array/AnagramsGroup.java
@@ -0,0 +1,33 @@
+package arithmetic.exercise.medium.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 字母异位词分组
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。
+ * 字母异位词指字母相同，但排列不同的字符串。
+ */
+public class AnagramsGroup {
+
+    private static List<List<String>> solution(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(solution(strs));
+    }
+
+}

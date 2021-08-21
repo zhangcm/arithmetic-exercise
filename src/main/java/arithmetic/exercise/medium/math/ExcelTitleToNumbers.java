30ada602e425af84eb0c2f9b9a10733d38edd680
zhangcm <justif_z@163.com>
2021-08-21 11:46:53 +0800

    excel表序列号

---
+++ src/main/java/arithmetic/exercise/medium/math/ExcelTitleToNumbers.java
@@ -0,0 +1,15 @@
+package arithmetic.exercise.medium.math;
+
+/**
+ * Excel表序列号
+ *
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ * 例如，A -> 1  B -> 2  C -> 3 ... Z -> 26 AA -> 27 AB -> 28
+ * 示例：
+ * A -> 1
+ * AB -> 28
+ * ZY -> 701
+ */
+public class ExcelTitleToNumbers {
+
+}

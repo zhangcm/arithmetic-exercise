5977eb23945f7810223a08b64a99750b526c6de0
zhangcm <justif_z@163.com>
2021-08-21 11:47:03 +0800

    两数相除

---
+++ src/main/java/arithmetic/exercise/medium/math/Divide.java
@@ -0,0 +1,16 @@
+package arithmetic.exercise.medium.math;
+
+/**
+ * 两数相除
+ * 给定两个整数，被除数dividend和除数divisor。将两数相除，要求不使用乘法、除法和mod运算法。
+ * 返回被除数dividend除以除数divisor得到的商。
+ * 整数除法的结果应当截去其小数部分，例如：truncate(8.345) = 8
+ * 示例：
+ * dividend = 10, divisor = 3  ->  3
+ * dividend = 7,  divisor = -3 -> -2
+ *
+ * 32位有符号整数，除数不为0，结果溢出则返回 2^31 - 1
+ */
+public class Divide {
+
+}

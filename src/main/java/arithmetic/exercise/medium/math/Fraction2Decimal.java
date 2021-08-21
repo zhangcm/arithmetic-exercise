ec7c2fbae01d863a2c37fb5ba6bb8a46f65bac0b
zhangcm <justif_z@163.com>
2021-08-21 11:50:42 +0800

    分数到小数

---
+++ src/main/java/arithmetic/exercise/medium/math/Fraction2Decimal.java
@@ -0,0 +1,18 @@
+package arithmetic.exercise.medium.math;
+
+/**
+ * 分数到小数
+ * 给定两个整数，分别表示分数的分子numerator和分母denominator，以字符串形式返回小数。
+ * 如果小数部分为循环小数，则将循环的部分括在括号内。如果存在多个答案，只需返回任意一个。
+ * 对于所有给定的输入，保证答案字符串的长度小于10^4
+ *
+ * 示例1：
+ * numerator = 1, denominator = 2 -> "0.5"
+ * numerator = 2, denominator = 1 -> "2"
+ * numerator = 2, denominator = 3 -> "0.(6)"
+ * numerator = 4, denominator = 333 -> "0.(012)"
+ * numerator = 1, denominator = 5 -> "0.2"
+ */
+public class Fraction2Decimal {
+
+}

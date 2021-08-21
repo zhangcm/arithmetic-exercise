8dd8446ff565453e73ce7e10f2b089021e583425
zhangcm <justif_z@163.com>
2021-08-21 11:48:07 +0800

    阶乘后的零

---
+++ src/main/java/arithmetic/exercise/medium/math/TrailingZeros.java
@@ -0,0 +1,15 @@
+package arithmetic.exercise.medium.math;
+
+/**
+ * 阶乘后的零
+ *
+ * 给定一个整数n，返回n!结果尾数中零的数量
+ *
+ * 示例：
+ * 3! -> 6 -> 0
+ * 5! -> 120 -> 1
+ * 算法的时间复杂度为O(logn)
+ */
+public class TrailingZeros {
+
+}

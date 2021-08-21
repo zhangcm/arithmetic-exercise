d9d1e797de82a5f6c4257f90c3adec7799b8919f
zhangcm <justif_z@163.com>
2021-08-21 11:49:56 +0800

    快乐数

---
+++ src/main/java/arithmetic/exercise/medium/math/HappyNumber.java
@@ -0,0 +1,25 @@
+package arithmetic.exercise.medium.math;
+
+/**
+ * 快乐数
+ *
+ * 编写一个算法来判断一个数n是不是快乐数。
+ * 「快乐数」定义为：
+ *  * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ *  * 然后重复这个过程直到这个数字变为1，也能是无限循环但始终变不到1。
+ *  * 如果可以变为1，那么这个数就是快乐数。
+ *  如果n是快乐数就返回true；不是，则返回false
+ *  示例：
+ *  19 -> true
+ *  1^2 + 9^2 = 82
+ *  8^2 + 2^2 = 68
+ *  6^2 + 8^2 = 100
+ *  1^2 + 0^2 + 0^2 = 1
+ */
+public class HappyNumber {
+
+    private static boolean solution(int n) {
+        return false;
+    }
+
+}

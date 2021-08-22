ab8cc29a996bc4538362d9ac777b514ad83ee389
zhangcm <justif_z@163.com>
2021-08-22 19:03:21 +0800

    TODO

---
+++ src/main/java/arithmetic/exercise/todo/senior/MaxWaterContainer.java
@@ -0,0 +1,25 @@
+package arithmetic.exercise.todo.senior;
+
+/**
+ * 盛最多水的容器
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+ *
+ * Notice that you may not slant the container.
+ *
+ * Example:
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ *
+ * Input: height = [4,3,2,1,4]
+ * Output: 16
+ */
+public class MaxWaterContainer {
+
+    private static int solution(int[] height) {
+        return 0;
+    }
+}

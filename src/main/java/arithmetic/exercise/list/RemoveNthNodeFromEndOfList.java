e9a0a74b8ff16e494cd404e5f6a63a58c2fb70c4
zhangcm <justif_z@163.com>
2021-09-08 10:17:20 +0800

    移除倒数第n个节点

--- src/main/java/arithmetic/exercise/list/RemoveNthNodeFromEndOfList.java
+++ src/main/java/arithmetic/exercise/list/RemoveNthNodeFromEndOfList.java
@@ -17,20 +17,33 @@ public class RemoveNthNodeFromEndOfList {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
         int index = length - n;
-        if (index == 0) {
-            dummy.next = head.next;
-        }
-        cur = head;
+        cur = dummy;
         int i = 0;
-        while (cur != null) {
-            if (i == index - 1) {
-                cur.next = cur.next.next;
-                break;
-            } else {
-                cur = cur.next;
-            }
+        while (i < index) {
+            cur = cur.next;
             i++;
         }
+        cur.next = cur.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 假如长度为length，倒数第n个就是整数第length - n个，第一个指针先走n步，然后第一个第二个指针同时走，
+     * 当第一个指针走到终点时，恰好走了length - n步。第二个指针也走了length - n步，执行删除。
+     */
+    private static ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p1 = dummy;
+        for (int i = 0; i < n; i++) {
+            p1 = p1.next;
+        }
+        ListNode p2 = dummy;
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p2.next = p2.next.next;
         return dummy.next;
     }
 }

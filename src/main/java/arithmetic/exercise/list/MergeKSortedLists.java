22ce5a9e11474c6bb5e0ffb77c68b45068bab135
zhangcm <justif_z@163.com>
2021-09-08 10:17:48 +0800

    合并有序链表

--- src/main/java/arithmetic/exercise/list/MergeKSortedLists.java
+++ src/main/java/arithmetic/exercise/list/MergeKSortedLists.java
@@ -1,6 +1,8 @@
 package arithmetic.exercise.list;
 
 import arithmetic.exercise.common.ListNode;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 23. Merge k Sorted Lists
@@ -10,20 +12,33 @@ public class MergeKSortedLists {
     /**
      * PriorityQueue
      */
-    public ListNode mergeKLists(ListNode[] lists) {
+    private static ListNode mergeKLists(ListNode[] lists) {
         ListNode dummy = new ListNode(-1);
-
-        return null;
+        ListNode cur = dummy;
+        int length = lists.length;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(length, Comparator.comparing(ListNode::getVal));
+        for (int i = 0; i < length; i++) {
+            queue.add(lists[i]);
+        }
+        ListNode min;
+        while (!queue.isEmpty()) {
+            min = queue.poll();
+            if (min != null) {
+                cur.next = min;
+                cur = cur.next;
+                queue.add(min.next);
+            }
+        }
+        return dummy.next;
     }
 
-    public ListNode mergeKLists2(ListNode[] lists) {
-        ListNode dummy = new ListNode(-1);
-
-        return null;
-    }
-
-    private ListNode mergeTwoList(ListNode l1, ListNode l2) {
-        return null;
+    private static ListNode mergeKLists1(ListNode[] lists) {
+        int length = lists.length;
+        ListNode cur = lists[0];
+        for (int i = 1; i < length; i++) {
+            cur = MergeTwoSortedLists.mergeTwoLists(cur, lists[i]);
+        }
+        return cur;
     }
 
 }
--- src/main/java/arithmetic/exercise/list/MergeTwoSortedLists.java
+++ src/main/java/arithmetic/exercise/list/MergeTwoSortedLists.java
@@ -7,7 +7,24 @@ import arithmetic.exercise.common.ListNode;
  */
 public class MergeTwoSortedLists {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+    /**
+     * 虚拟头结点，简化处理
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            cur.next = l1.val <= l2.val ? l1 : l2;
+            cur = cur.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return dummy.next;
     }
 }

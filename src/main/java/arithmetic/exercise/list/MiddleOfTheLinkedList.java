df25f09ac85ae8183ed1eaa1822bbe22a5c4cbd5
zhangcm <justif_z@163.com>
2021-09-08 10:18:07 +0800

    链表中点

--- src/main/java/arithmetic/exercise/list/MiddleOfTheLinkedList.java
+++ src/main/java/arithmetic/exercise/list/MiddleOfTheLinkedList.java
@@ -8,6 +8,33 @@ import arithmetic.exercise.common.ListNode;
 public class MiddleOfTheLinkedList {
 
     private static ListNode middleNode(ListNode head) {
-        return null;
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (fast != null) {
+            slow = slow.next;
+            // 长度为偶数
+            if (fast.next == null) {
+                break;
+            }
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        for (int length = 1; length < 10; length++) {
+            ListNode head = new ListNode(1);
+            ListNode cur = head;
+            for (int i = 1; i < length; i++) {
+                cur.next = new ListNode(i + 1);
+                cur = cur.next;
+            }
+            System.out.println(middleNode(head).val);
+        }
     }
 }

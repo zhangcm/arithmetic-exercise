30f02c15b090e896b908b1383b31f6ec34ca489f
zhangcm <justif_z@163.com>
2021-10-08 10:51:11 +0800

    Russian Doll Envelopes

--- src/main/java/arithmetic/exercise/todo/dp/RussianDollEnvelopes.java
+++ src/main/java/arithmetic/exercise/todo/dp/RussianDollEnvelopes.java
@@ -1,5 +1,9 @@
 package arithmetic.exercise.todo.dp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 354. Russian Doll Envelopes [hard]
  *
@@ -17,10 +21,58 @@ package arithmetic.exercise.todo.dp;
 public class RussianDollEnvelopes {
 
     /**
+     * TODO run
+     */
+    /**
      * 先对宽度 w 进行升序排序，如果遇到 w 相同的情况，则按照高度 h 降序排序。之后把所有的 h 作为一个数组，
      * 在这个数组上计算 LIS 的长度就是答案。
      */
     private static int maxEnvelopes(int[][] envelopes) {
-        return 0;
+        Arrays.sort(envelopes, (o1, o2) -> {
+            if (o1[0] > o2[0]) {
+                return 1;
+            } else if (o1[0] == o2[0]) {
+                if (o1[1] > o2[1]) {
+                    return -1;
+                } else if (o1[1] == o2[1]) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else {
+                return -1;
+            }
+        });
+        int length = envelopes.length;
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = envelopes[i][1];
+        }
+        return lengthOfLIS1(result);
     }
+
+    private static int lengthOfLIS1(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            int index = -1;
+            for (int i = 0, size = list.size(); i < size; i++) {
+                if (num <= list.get(i)) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1) {
+                list.add(num);
+            } else {
+                list.set(index, num);
+            }
+        }
+        return list.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxEnvelopes(new int[][] {{5, 4}, {6, 4}, {6, 7}, {2, 3}}));  // 3
+        System.out.println(maxEnvelopes(new int[][] {{1, 1}, {1, 1}, {1, 1}}));  // 1
+    }
+
 }
